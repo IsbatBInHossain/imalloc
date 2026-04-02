bb0cef8efbadf54fb449c3eae2803a014d612ceb
Isbat <isbat31416@gmail.com>
2026-04-02 19:49:22 +0600

    add simple free list

--- src/malloc.c
+++ src/malloc.c
@@ -1,18 +1,77 @@
 #include <stdio.h>
 #include <unistd.h>
 
+typedef struct block
+{
+  size_t size;
+  int free;
+  struct block *next;
+} block_t;
+
+block_t *head = NULL;
+
 void *imalloc(size_t size)
 {
-  void *ptr = sbrk(size);
-  if (ptr == (void *)-1)
+  void *request = sbrk(sizeof(block_t) + size);
+  if (request == (void *)-1)
     return NULL;
-  return ptr;
-};
-// void ifree(void *ptr);
+
+  block_t *block = (block_t *)request;
+
+  block->size = size;
+  block->free = 0;
+  block->next = NULL;
+
+  // Add to linked list
+  if (head == NULL)
+  {
+    head = block;
+  }
+  else
+  {
+    block_t *curr = head;
+    while (curr->next != NULL)
+      curr = curr->next;
+
+    curr->next = block;
+  }
+
+  return (void *)(block + 1);
+}
+
+void ifree(void *ptr)
+{
+  if (!ptr)
+    return;
+
+  block_t *block = (block_t *)ptr - 1;
+  block->free = 1;
+
+  printf("Freed block:\n");
+  printf("address : %p\n", (void *)block);
+  printf("size    : %zu\n", block->size);
+}
+
+void print_heap()
+{
+  block_t *curr = head;
+  while (curr)
+  {
+    printf("[addr=%p size=%zu free=%d] -> ",
+           (void *)curr, curr->size, curr->free);
+    curr = curr->next;
+  }
+  printf("NULL\n");
+}
 
 int main()
 {
   int *x = imalloc(sizeof(int));
-  *x = 42;
-  printf("%d\n", *x);
+  int *y = imalloc(sizeof(int));
+
+  print_heap();
+
+  ifree(x);
+
+  print_heap();
 }
